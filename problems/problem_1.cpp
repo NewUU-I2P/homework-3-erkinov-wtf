bd4a7551cc75253ccae8abe2ab2ea877cde10469
Erkinov <erkinovit@gmail.com>
2023-11-10 18:51:35 +0500

    first commit

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,15 +2,34 @@ float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
 
-    if (consumed_water <= 30) {
-        cost = 13 + consumed_water * 0.4;
-    } else if (consumed_water <= 50) {
-        cost = 13 + 30 * 0.4 + (consumed_water - 30) * 0.12;
-    } else if (consumed_water <= 60) {
-        cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
+    cost = 13; // Fixed amount
+    double remaining = consumed_water;
+
+    if (remaining > 30) {
+        cost += 30 * 0.4;
+        remaining -= 30;
     } else {
-        cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
+        cost += remaining * 0.4;
+        return cost;
     }
+
+    if (remaining > 20) {
+        cost += 20 * 0.12;
+        remaining -= 20;
+    } else {
+        cost += remaining * 0.12;
+        return cost;
+    }
+
+    if (remaining > 10) {
+        cost += 10 * 1.4;
+        remaining -= 10;
+    } else {
+        cost += remaining * 1.4;
+        return cost;
+    }
+
+    cost += remaining * 1.5;
 
     return cost;
 }
