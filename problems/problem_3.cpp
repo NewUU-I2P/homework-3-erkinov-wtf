8a23055f85e03e6cdfe4062bb444ec82c601feb1
Erkinov <erkinovit@gmail.com>
2023-11-10 18:46:05 +0500

    first commit

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -4,29 +4,25 @@ std::string problemSolution3(float height, char S) {
     // write your code here
     std::string result;
 
-    switch (S == 'M') {
-        case 1:
-            if (height < 1.70) {
-                result = "Short";
-            } else if (height >= 1.70 && height <= 1.85) {
-                result = "Normal";
-            } else if (height >= 1.85) {
-                result = "Tall";
-            }
-            break;
-        case 0:
-            switch (S == 'F') {
-                case 1:
-                    if (height < 1.60) {
-                        result = "Short";
-                    } else if (height >= 1.60 && height <= 1.75) {
-                        result = "Normal";
-                    } else if (height >= 1.75) {
-                        result = "Tall";
-                    }
-                    break;
-                case 0:
-                    result = "Undefined";
-            }
+    if (S == 'M') {
+        if (height < 1.70) {
+            result = "Short";
+        } else if (height >= 1.70 && height < 1.85) {
+            result = "Normal";
+        } else {
+            result = "Tall";
+        }
+    } else if (S == 'F') {
+        if (height < 1.60) {
+            result = "Short";
+        } else if (height >= 1.60 && height < 1.75) {
+            result = "Normal";
+        } else {
+            result = "Tall";
+        }
+    } else {
+        result = "Invalid input for sex.";
     }
+
+    return result;
 }
